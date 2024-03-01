c96c709b3ad71732bc5676924c9acdead566d884
Santiago Ferreira <santif028@gmail.com>
2024-03-01 02:52:03 -0300

    cambios en el middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { NextResponse, NextRequest } from 'next/server'
-import { apiAuthPrefix, dashboardRoutesPrefix, publicRoutes, authRoutes } from './routes';
+import { apiAuthPrefix, dashboardRoutesPrefix, publicRoutes, authRoutes, DEFAULT_LOGIN_REDIRECT_URL } from './routes';
 
 export async function middleware(request: NextRequest) {
     let response = NextResponse.next({
@@ -8,31 +8,6 @@ export async function middleware(request: NextRequest) {
             headers: request.headers,
         },
     })
-    const isApiAuthRoute = request.nextUrl.pathname.startsWith(apiAuthPrefix);
-    const isPublicRoute = publicRoutes.includes(request.nextUrl.pathname);
-    const isDashboardRoute = request.nextUrl.pathname.startsWith(dashboardRoutesPrefix);
-    const isAuthRoute = authRoutes.includes(request.nextUrl.pathname);
-    const short_url = request.nextUrl.pathname.split('/').pop();
-
-
-    if (!isDashboardRoute && !isApiAuthRoute && !isAuthRoute) {
-        const data = await fetch(
-            `${request.nextUrl.origin}/api/url?short_url=${short_url}`,
-        );
-
-        if (data.status === 404) {
-            console.log(`Short URL not found: ${short_url}`);
-            return null;
-        }
-
-        const dataToJson = await data.json();
-
-        if (dataToJson.original_url) {
-            return Response.redirect(new URL(dataToJson.original_url as string).toString());
-        }
-
-    }
-
     const supabase = createServerClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -79,7 +54,43 @@ export async function middleware(request: NextRequest) {
         }
     )
 
-    await supabase.auth.getUser()
+    const user = await supabase.auth.getUser()
+
+    const isApiAuthRoute = request.nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.includes(request.nextUrl.pathname);
+    const isDashboardRoute = request.nextUrl.pathname.startsWith(dashboardRoutesPrefix);
+    const isAuthRoute = authRoutes.includes(request.nextUrl.pathname);
+    const short_url = request.nextUrl.pathname.split('/').pop();
+
+    if (isApiAuthRoute) {
+        return null;
+    }
+
+    if (isAuthRoute && user) {
+        return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT_URL, request.nextUrl));
+    }
+    
+    if (!user && !isPublicRoute) {
+        return Response.redirect(new URL("/login", request.nextUrl));
+    }
+
+    if (!isDashboardRoute && !isApiAuthRoute && !isAuthRoute) {
+        const data = await fetch(
+            `${request.nextUrl.origin}/api/url?short_url=${short_url}`,
+        );
+
+        if (data.status === 404) {
+            console.log(`Short URL not found: ${short_url}`);
+            return null;
+        }
+
+        const dataToJson = await data.json();
+
+        if (dataToJson.original_url) {
+            return Response.redirect(new URL(dataToJson.original_url as string).toString());
+        }
+
+    }
 
     return response
 }

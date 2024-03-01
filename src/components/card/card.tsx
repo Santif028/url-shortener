610bdd6cd6bf54fca5d55105906ff4b65125a476
Santiago Ferreira <santif028@gmail.com>
2024-03-01 04:11:26 -0300

    copy to clipboard arreglado y redireccionamiento del dashboard corregido

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -64,9 +64,9 @@ export const Card = (props: CardProps) => {
         <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 overflow-hidden">
 
             <div className="flex items-center gap-5 mb-2">
-                <Link href={`http://localhost:3000/s/${props.short_url}`} target="_blank" className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{`/s/${props.short_url}`}</Link>
+                <Link href={`https://setsu-url-shortener.vercel.app/s/${props.short_url}`} target="_blank" className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{`/s/${props.short_url}`}</Link>
                 <button title="Copy to clipboard" className="flex size-5 items-center justify-center text-gray-700 dark:text-gray-400 hover:scale-110 hover:text-white" onClick={() =>
-                    copyToClipboard(`https://slug.vercel.app/s/${props.short_url}`)
+                    copyToClipboard(`https://setsu-url-shortener.vercel.app/s/${props.short_url}`)
                 }>
                     <CopyIcon />
                 </button>
